99ef4dcc229f7195b0df83965bfd2b03155ff44a
Jon Bai <jowned1@gmail.com>
2023-05-01 20:05:44 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Hero } from './components/Hero';
+import { Who } from './components/Who';
+import { Contact } from './components/Contact';
+import { Works } from './components/Works';
+import styled from 'styled-components';
+
+
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  color: white;
+  background: url("./img/bg.jpeg");
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  );
+}
+
+export default App;
